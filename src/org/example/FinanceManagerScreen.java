2d6639b600911144c7f7d4ad9b069cc449542513
MBarion <murilo.antonio.barion@gmail.com>
2025-03-29 14:46:59 -0300

    Integrando back com o java swing

--- src/org/example/FinanceManagerScreen.java
+++ src/org/example/FinanceManagerScreen.java
@@ -1,7 +1,16 @@
 package org.example;
 
 import javax.swing.*;
+import javax.swing.text.MaskFormatter;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class FinanceManagerScreen extends JFrame {
     private JTextField valueField, dateField, descriptionField;
@@ -30,13 +39,14 @@ public class FinanceManagerScreen extends JFrame {
         mainPanel.setLayout(new BorderLayout(10, 10));
         mainPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
+        // --------------------------- FORMULÁRIO DE CADASTRO DE TRANSACOES ---------------------------
         // Painel para o formulário de transações
         JPanel formPanel = new JPanel();
         formPanel.setLayout(new GridLayout(6, 2, 10, 10));
         formPanel.setBorder(BorderFactory.createTitledBorder("Cadastro de Transações"));
 
         formPanel.add(new JLabel("Tipo:"));
-        typeComboBox = new JComboBox<>(new String[]{"Receita (+)", "Despesa (-)"});
+        typeComboBox = new JComboBox<>(new String[]{"Receita", "Despesa"});
         formPanel.add(typeComboBox);
 
         formPanel.add(new JLabel("Categoria:"));
@@ -44,7 +54,14 @@ public class FinanceManagerScreen extends JFrame {
         formPanel.add(categoryComboBox);
 
         formPanel.add(new JLabel("Data:"));
-        dateField = new JTextField();
+        try {
+            MaskFormatter dateFormatter = new MaskFormatter("##/##/####");
+            dateFormatter.setPlaceholderCharacter('_');
+            dateField = new JFormattedTextField(dateFormatter);
+            dateField.setColumns(10);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
         formPanel.add(dateField);
 
         formPanel.add(new JLabel("Descrição:"));
@@ -57,14 +74,46 @@ public class FinanceManagerScreen extends JFrame {
 
         JButton addTransactionButton = new JButton("Adicionar Transação");
         addTransactionButton.setPreferredSize(new Dimension(10, 30));
+        addTransactionButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // (String) faz o cast para o tipo String
+                String tipo = (String) typeComboBox.getSelectedItem();
+                String categoria = (String) categoryComboBox.getSelectedItem();
+                String data = (String) dateField.getText();
+                String descricao = (String) descriptionField.getText();
+                String valor = (String) valueField.getText();
+
+                if (tipo == null || data == null || descricao == null || valor == null) {
+                    JOptionPane.showMessageDialog(null, "Preencha todos os campos!", "Erro", JOptionPane.ERROR_MESSAGE);
+                    return;
+                }else {
+                    // Definindo o formato esperado
+                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+                    Transacao transaction = new Transacao(tipo, new Categoria(categoria), LocalDate.parse(data, formatter), descricao, Double.parseDouble(valor));
+                    user.adicionarTransacao(transaction);
+
+                    JOptionPane.showMessageDialog(FinanceManagerScreen.this, "Cadastro realizado com sucesso!", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
+                    new FinanceManagerScreen(user);
+                    dispose();
+                }
+
+            }
+        });
         formPanel.add(addTransactionButton);
 
+        // --------------------------- PAINÉIS DE HISTÓRICO E RESUMO ---------------------------
         // Painel para o histórico de transações
         JPanel historyPanel = new JPanel();
         historyPanel.setLayout(new BorderLayout());
         historyPanel.setBorder(BorderFactory.createTitledBorder("Histórico de Transações"));
 
-        JList<String> transactionList = new JList<>();
+        List<String> transactionDescriptions = new ArrayList<>();
+        for (Transacao transacao : user.getTransacoes()) {
+            transactionDescriptions.add(transacao.getDescricao()); // ou qualquer outro campo que seja String
+        }
+
+        JList<String> transactionList = new JList<>(transactionDescriptions.toArray(new String[0]));
         JScrollPane scrollPane = new JScrollPane(transactionList);
         historyPanel.add(scrollPane, BorderLayout.CENTER);
 
@@ -81,18 +130,33 @@ public class FinanceManagerScreen extends JFrame {
         summaryPanel.add(incomeLabel);
         summaryPanel.add(expenseLabel);
 
+        // ---------------------------- PAINEL DE CATEGORIAS ---------------------------
         // Painel para gerenciamento de categorias
         JPanel categoryPanel = new JPanel();
         categoryPanel.setLayout(new BorderLayout(10, 10));
         categoryPanel.setBorder(BorderFactory.createTitledBorder("Gerenciamento de Categorias"));
 
-        JTextField newCategoryField = new JTextField();
-        newCategoryField.setPreferredSize(new Dimension(100, 150));
-        for (int i = 0; i < 10; i++) {
-            newCategoryField.setText("Categoria " + i);
+        List<Categoria> categories = user.getCategorias();
+
+        JPanel categoryContainer = new JPanel();
+        categoryContainer.setLayout(new BoxLayout(categoryContainer, BoxLayout.Y_AXIS)); // Organiza os campos verticalmente
+
+        if(categories != null && !categories.isEmpty() ) {
+            for (Categoria categoria : categories) {
+                JTextField newCategoryField = new JTextField(categoria.getDescricao());
+                newCategoryField.setMaximumSize(new Dimension(Integer.MAX_VALUE, 30)); // Ajusta a largura
+                categoryContainer.add(newCategoryField);
+            }
         }
-//        newCategoryField.setText("Categoria");
-        categoryPanel.add(newCategoryField, BorderLayout.NORTH);
+
+        // Adiciona o painel dentro de um JScrollPane
+        JScrollPane categoryScrollPane = new JScrollPane(categoryContainer);
+        categoryScrollPane.setPreferredSize(new Dimension(150, 180)); // Ajuste conforme necessário
+        categoryScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        categoryScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+
+        // Adiciona o JScrollPane ao painel principal onde estava seu campo de texto original
+        categoryPanel.add(categoryScrollPane, BorderLayout.NORTH);
 
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 5));
@@ -129,5 +193,4 @@ public class FinanceManagerScreen extends JFrame {
         add(mainPanel);
         setVisible(true);
     }
-
 }
